f0777e7c9afde42e512ffd5d260d775e97ab22a2
Hartono Chandra <harchcode@gmail.com>
2021-04-24 21:57:03 +0700

    delete dist

--- dist/dom/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare type Child = HTMLElement | SVGElement;
-declare type Children = string | null | Child | Child[];
-export declare function el<T extends HTMLElement>(tag: keyof HTMLElementTagNameMap, attrs: {
-    [x in keyof T]?: T[x];
-}, children?: Children): T;
-export {};
